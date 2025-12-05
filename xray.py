084ecd5b4c1df0ab9db89a59fc9346eb102093df
Line <159905927+linetoemmerup@users.noreply.github.com>
2025-12-05 18:23:29 +0100

    Update xray.py

--- xray.py
+++ xray.py
@@ -1,14 +1,1 @@
-import pydicom
-import matplotlib.pyplot as plt
 
-# Load X-ray DICOM file
-ds = pydicom.dcmread("xray/IM-0003-0001.dcm")
-img = ds.pixel_array
-
-# Display raw X-ray image
-plt.figure(figsize=(6,6))
-plt.imshow(img, cmap="gray")
-plt.title("Raw X-ray - A8")
-plt.axis("off")
-plt.savefig("xray_raw_A8.png", dpi=300, bbox_inches="tight")
-plt.show()
